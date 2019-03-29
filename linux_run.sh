291634ddd87d591762a49cb3a58aeed71a3ea0d8
fwfly <fawafwfly@gmail.com>
2019-03-29 21:03:09 +1100

    Linux run scripts

---
+++ linux_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+sudo apt-get install qemu libncurses5-dev gcc-arm-linux-gnueabi build-essential
+wget https://busybox.net/downloads/busybox-1.24.0.tar.bz2
+wget https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.0.tar.xz
+
